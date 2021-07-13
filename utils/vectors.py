ab5e4fabdfbba9abecf47eeda7327c18cd1291d4
heidar <heydar.fahmi@sokhan.ai>
2021-07-13 18:34:42 +0430

    add score_matrix function

--- utils/vectors.py
+++ utils/vectors.py
@@ -1,4 +1,5 @@
 import math
+import numpy
 
 
 def cal_doc_tf(number):
@@ -9,6 +10,32 @@ def cal_idf(nt, N):
     return math.log(float(N) / nt)
 
 
+def normalize_vector(array):
+    vector = numpy.asarray(array, dtype=numpy.float64).reshape(len(array), 1)
+    return vector / numpy.linalg.norm(vector)
+
+
+def make_vector(tokens, arg="tf"):
+    if arg == "tf":
+        array = []
+        for token in tokens:
+            array.append(tokens[token]['tf'])
+        return normalize_vector(array)
+
+
+def make_docs_vector(query_tokens, N, all_tokens):
+    score_matrix = numpy.zeros((N + 1, len(query_tokens)), dtype=numpy.float64)
+    for column_id, token in enumerate(query_tokens):
+        tf_idf_per_doc = all_tokens.get(token, {})
+        for doc in tf_idf_per_doc:
+            score_matrix[doc][column_id] = tf_idf_per_doc[doc]['tf_idf']
+    return score_matrix
+
+
+def cal_scores(matrix_score, query_vector):
+    return numpy.dot(matrix_score, query_vector)
+
+
 def vectorize_doc(tokens):
     'cal tf of each token in document'
     for token in tokens:
@@ -16,7 +43,7 @@ def vectorize_doc(tokens):
     return tokens
 
 
-def vectorizing_docs(tokens, number_of_docs):
+def vectorizing_docs(tokens, number_of_docs, docs_norms):
     for token in tokens:
         docs = tokens[token]
         idf = cal_idf(len(docs.keys()), number_of_docs)
@@ -24,7 +51,15 @@ def vectorizing_docs(tokens, number_of_docs):
         docs = {doc_id: {"occur": occur_num, "tf_idf": cal_doc_tf(occur_num) * idf} for doc_id, occur_num in
                 docs}  # cal tf for docs
 
+        for doc_id in docs.keys():
+            docs_norms[doc_id] += (token[doc_id]['tfi_idf'] ** 2)
+        numpy.asarray(token["tf_idf"])
+
         tokens[token] = docs
 
-    return token
+    for token in tokens:
+        docs = tokens[token]
+        docs = {doc_id: {"occur": docs[doc_id]['occur'], "tf_idf": docs[doc_id] / math.sqrt(docs_norms[doc_id])}}
+        tokens[token] = docs
 
+    return tokens
